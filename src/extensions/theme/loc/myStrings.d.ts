1853bf1212d7bda979975d642a0d010f88b079fc
Daniel Stratton <dstratton@lsijax.com>
2020-03-03 10:58:18 -0500

    initial

---
+++ src/extensions/theme/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IThemeApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ThemeApplicationCustomizerStrings' {
+  const strings: IThemeApplicationCustomizerStrings;
+  export = strings;
+}
